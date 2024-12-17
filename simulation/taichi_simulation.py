1147b7474f2b47f00f21df6db2a918a397156738
wycui <125980676+Agonian-DB@users.noreply.github.com>
2024-12-16 20:38:51 -0500

    Update taichi_simulation.py

--- simulation/taichi_simulation.py
+++ simulation/taichi_simulation.py
@@ -15,8 +15,6 @@ class TaichiSimulation:
         self.num_springs = spring_indices.shape[0]
         self.dt = dt
         self.num_steps = num_steps
-
-        # 将粒子位置和弹簧数据转换为 Taichi 字段
         self.x = ti.Vector.field(self.dim, dtype=ti.f32, shape=(self.num_steps + 1, self.num_particles))
         self.v = ti.Vector.field(self.dim, dtype=ti.f32, shape=self.num_particles)
         self.f = ti.Vector.field(self.dim, dtype=ti.f32, shape=self.num_particles)
@@ -25,35 +23,29 @@ class TaichiSimulation:
         self.rest_length = ti.field(dtype=ti.f32, shape=self.num_springs)
         self.k_spring = ti.field(dtype=ti.f32, shape=self.num_springs, needs_grad=True)
 
-        # 声明 positions_gt 作为 Taichi 字段
         self.positions_gt = ti.Vector.field(self.dim, dtype=ti.f32, shape=(self.num_steps + 1, self.num_particles))
         self.loss = ti.field(dtype=ti.f32, shape=(), needs_grad=True)
 
-        # 初始化
         self.initialize(particle_positions, spring_indices, spring_k_values, positions_gt)
 
     def initialize(self, particle_positions, spring_indices, spring_k_values, positions_gt):
-        # 初始化粒子位置
         self.x[0].from_numpy(particle_positions)
         self.v.fill(0.0)
 
-        # 初始化弹簧
         self.springs.from_numpy(spring_indices)
         for i in range(self.num_springs):
             a, b = self.springs[i]
             self.rest_length[i] = (self.x[0, a] - self.x[0, b]).norm()
             self.k_spring[i] = spring_k_values[i]
 
-        # 将 positions_gt 从 NumPy 数组加载到 Taichi 字段
         self.positions_gt.from_numpy(positions_gt)
 
     @ti.kernel
     def substep(self, t: ti.i32):
-        # 计算粒子受力
+
         for i in range(self.num_particles):
             self.f[i] = self.mass * self.gravity
 
-        # 计算弹簧力
         for i in range(self.num_springs):
             a, b = self.springs[i]
             x_a = self.x[t, a]
@@ -67,13 +59,11 @@ class TaichiSimulation:
                 self.f[a] += force
                 self.f[b] -= force
 
-        # 更新粒子位置和速度
         for i in range(self.num_particles):
             self.v[i] += self.dt * self.f[i] / self.mass
             self.v[i] *= ti.exp(-self.dt * self.damping)
             self.x[t + 1, i] = self.x[t, i] + self.dt * self.v[i]
 
-        # 固定顶部粒子
         for i in range(self.num_particles):
             if self.x[t + 1, i].y > 0.95:
                 self.v[i] = ti.Vector([0.0, 0.0])
@@ -85,7 +75,6 @@ class TaichiSimulation:
                 self.substep(t)
             self.compute_loss()
 
-        # 提取粒子位置序列
         positions_over_time = [self.x[t].to_numpy() for t in range(self.num_steps + 1)]
         return positions_over_time
 
